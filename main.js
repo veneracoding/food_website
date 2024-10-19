c603929fb62e723b77e8ea4bea96613e8b44c1bc
veneracoding <otaboyevashahzoda0405@gmail.com>
2024-10-19 22:17:43 +0500

    ends

--- main.js
+++ main.js
@@ -1,64 +1,65 @@
 $(document).ready(function ($) {
     "use strict";
 
-    var book_table=new Swiper(".book-table-img-slider",{
-        slidesPerView:1,
-        spaceBetween:20,
-        loop:true,
-        autoplay:{
-            delay:3000,
-            disableOnInteraction:false,
+
+    var book_table = new Swiper(".book-table-img-slider", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
         },
-        speed:2000,
-        effect:"coverflow",
-        coverlfowEffect:{
-            rotate:3,
-            stretch:2,
-            depth:100,
-            modifier:5,
-            slideShadows:false,
+        speed: 2000,
+        effect: "coverflow",
+        coverflowEffect: {
+            rotate: 3,
+            stretch: 2,
+            depth: 100,
+            modifier: 5,
+            slideShadows: false,
         },
-        loopAdditionSlides:true,
-        navigation:{
-            nextEl:".swiper-button-next",
-            prevEl:".swiper-button-prev"
+        loopAdditionSlides: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
-        pagination:{
-            el:".swiper-pagination",
-            clickable:true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
     });
 
-    var team_slider=new Swiper(".team-slider",{
-        slidesPerView:3,
-        spaceBetween:30,
-        loop:true,
-        autoplay:{
-            delay:3000,
-            disableOnInteraction:false,
+    var team_slider = new Swiper(".team-slider", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
         },
-        speed:2000,
-       
-        navigation:{
-            nextEl:".swiper-button-next",
-            prevEl:".swiper-button-prev"
+        speed: 2000,
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
         },
-        pagination:{
-            el:".swiper-pagination",
-            clickable:true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
         },
-        breakpoints:{
-            0:{
-               slidesPerView:1.2, 
+        breakpoints: {
+            0: {
+                slidesPerView: 1.2,
             },
-            768:{
-                slidesPerView:2,
+            768: {
+                slidesPerView: 2,
             },
-            992:{
-                slidesPerView:3,
+            992: {
+                slidesPerView: 3,
             },
-            1200:{
-                slidesPerView:3,
+            1200: {
+                slidesPerView: 3,
             },
         },
     });
@@ -87,55 +88,53 @@ $(document).ready(function ($) {
         filterList.init();
     });
 
-    jQuery(".menu-toggle").click(function(){
+    jQuery(".menu-toggle").click(function () {
         jQuery(".main-navigation").toggleClass("toggled");
     });
 
-    jQuery(".header-menu ul li a").click(function(){
+    jQuery(".header-menu ul li a").click(function () {
         jQuery(".main-navigation").removeClass("toggled");
     });
 
     gsap.registerPlugin(ScrollTrigger);
 
-    var elementFirst=document.querySelector('.site-header');
+    var elementFirst = document.querySelector('.site-header');
     ScrollTrigger.create({
-        trigger:"body",
+        trigger: "body",
         start: "30px top",
         end: "bottom bottom",
 
-        onEnter: ()=>myFunction(),
-        onLeaveBack: ()=>myFunction(),
+        onEnter: () => myFunction(),
+        onLeaveBack: () => myFunction(),
     });
 
-    function myFunction(){
+    function myFunction() {
         elementFirst.classList.toggle('sticky_head');
     }
 
     var scene = $(".js-parallax-scene").get(0);
     var parallaxInstance = new Parallax(scene);
 
+
 });
 
 
-jQuery(window).on('load', function(){
-    $('body').removeClass('body-fixed')
+jQuery(window).on('load', function () {
+    $('body').removeClass('body-fixed');
 
-    // activiting tab of filter
-    let targets=document.querySelectorAll(".filter");
-    let activeTab=0;
-    let old=0;
-    let dur=0.4;
+    //activating tab of filter
+    let targets = document.querySelectorAll(".filter");
+    let activeTab = 0;
+    let old = 0;
+    let dur = 0.4;
     let animation;
 
-    for (let i = 0; i< targets.length; i++) {
+    for (let i = 0; i < targets.length; i++) {
         targets[i].index = i;
         targets[i].addEventListener("click", moveBar);
-        
-    };
+    }
 
-
-    // initial position on first===All
-
+    // initial position on first === All 
     gsap.set(".filter-active", {
         x: targets[0].offsetLeft,
         width: targets[0].offsetWidth
@@ -171,4 +170,3 @@ jQuery(window).on('load', function(){
 
     }
 });
-
